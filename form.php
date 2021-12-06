d1c4828571df8e337e4fed6b847bfcee34ae5bb3
Fozan Bebe Moreno <93265658+fbebemoreno@users.noreply.github.com>
2021-12-06 19:44:17 +0700

    Update form.php

--- form.php
+++ form.php
@@ -1,49 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+    include_once 'header.php';
+?>
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-
-    <title>Survey Vaksinasi - Main</title>
-</head>
-
-<body>
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700&display=swap"
-        rel="stylesheet" />
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
-
-    <!-- Navbar -->
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fw-bold" style="background-color: #f5f5f5">
-        <div class="container" style="font-family: 'Manrope', sans-serif">
-            <ul class="navbar-nav">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        Menu
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
-                        <li><a class="dropdown-item" href="result.php">Dashboard</a></li>
-                    </ul>
-                </li>
-            </ul>
-
-            <span class="navbar-text"> Survey Vaksinasi</span>
-        </div>
-    </nav>
 
     <div class="container-fluid" style="background-color: #f5f5f5">
         <br /><br /><br />
@@ -140,8 +99,9 @@
         </div>
         <br /><br /><br />
     </div>
-</body>
 
 
 
-</html>
+<?php
+    include_once 'footer.php';
+?>
