5ce29132dfcae3c984779b35a5ed78af5b9e7809
Fozan Bebe Moreno <93265658+fbebemoreno@users.noreply.github.com>
2021-12-06 19:46:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-
-    <title>Survey Vaksinasi - Main</title>
-</head>
-
-<body>
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700&display=swap"
-        rel="stylesheet" />
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
+<?php
+    include_once 'header.php';
+?>
+    
     <div class="container-fluid" style="background-color: #f5f5f5">
         <br /><br /><br />
         <div class="container" style="font-family: 'Manrope', sans-serif">
@@ -45,14 +23,27 @@
                     </div>
                     <br><br><br><br><br><br><br><br><br>
                     <div class="mb-3 row">
-                        <center><a href="form.php"><button type="submit" class="btn btn-primary fs-5 text">Isi
-                                    Survey</button></a></center>
+
+                        <?php
+                            if (isset($_SESSION['useruid'])) {
+                                echo "<center><a href=\"form.php\"><button type=\"submit\" class=\"btn btn-primary fs-5 text\">Isi
+                                Survey</button></a></center>";
+                            }
+                            else {
+                                echo "<p class=\"text-center\">Anda belum login, silahkan <span><a href=\"login.php\" class=\"link-success\">Login</a></span> untuk mengisi form survey.</p>
+                                <p class=\"text-center\">Anda belum punya akun? Ayo <span><a href=\"signup.php\" class=\"link-warning\">Buat Akun</a></span> sekarang!</p>";
+                            }
+                        ?>
+
                     </div>
                 </div>
             </div>
         </div>
         <br /><br /><br />
     </div>
-</body>
 
-</html>
+
+    
+<?php
+    include_once 'footer.php';
+?>
