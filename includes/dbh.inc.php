7097d19ad778f30588370995d778ea5334ea9ac8
zhafranP <91034652+zhafranP@users.noreply.github.com>
2021-12-14 19:41:28 +0700

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,11 +1,11 @@
 <?php
 
-$serverName = "sql312.epizy.com";
-$dbUsername = "epiz_30479689";
-$dbPassword = "79v6g4XIdSSz";
-$dbName = "epiz_30479689_survey_vaksinasi";
+$host = 'sql312.epizy.com';
+$dbusername = 'epiz_30479689';
+$password = '79v6g4XIdSSz';
+$database = 'epiz_30479689_survey_vaksinasi';
 
-$conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName);
+$conn = mysqli_connect($host, $dbusername, $password, $database);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
